0fb3f068c4a13de743732b77f80d24b25422162e
Rich Doan <richd@indeed.com>
2016-01-09 11:05:21 -0600

    add utils module

---
+++ juglechart/utils.py
@@ -0,0 +1,44 @@
+'''
+Created on Jan 9, 2016
+
+@author: richd
+'''
+
+import random 
+import pandas as pd
+
+import numpy as np
+
+
+
+def get_random_int():
+
+    pass
+
+def sample_date_sequence(periods):
+    
+    rng = pd.date_range('1/1/2015', periods=periods, freq='D')
+
+    return rng
+
+
+def get_random_date_data(days=4, num_series=4):
+
+    data = {}
+    data['dates'] = sample_date_sequence(days)
+    label_num = 65 # ascii 'A'
+    
+    for num in range(num_series):
+        
+        label = chr(label_num)
+        mu = random.randint(20, 51)
+        sd = 2
+        s = np.random.normal(mu, sd, days)
+        data[label] = s
+
+        
+    return pd.DataFrame(data)
+
+
+print get_random_date_data(10)
+
