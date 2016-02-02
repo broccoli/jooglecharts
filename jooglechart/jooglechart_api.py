73148fd285f4e9b77c3855181869f18efd73f4f2
Patrick Fennelly <pfennelly@indeed.com>
2016-02-02 15:11:39 -0600

    typofix

--- jooglechart/jooglechart_api.py
+++ jooglechart/jooglechart_api.py
@@ -789,7 +789,7 @@ def _get_nbparams(frame_globals):
 def _get_notebook_url():
     import urllib
     fg = _frame_globals()
-    if _is_real_ishbook(frame_globals):
+    if _is_real_ishbook(fg):
         (nbid, nbparams) = (_get_nbid(fg), _get_nbparams(fg))
         qs = urllib.urlencode(nbparams, doseq=True)
         return BASE_NOTEBOOK_URL.format(nbid=nbid, qs=qs)
