f823687c386229c9f07133fbff16264129d51a10
Rich Doan <richd@indeed.com>
2016-02-12 07:12:07 -0600

    advance version in setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='jooglechart',
-    version='0.1',
+    version='0.1.1',
     packages=['jooglechart',],
     package_data={'jooglechart': ['jinja_templates/*.html', '*.ipynb']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
