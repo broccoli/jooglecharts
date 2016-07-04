6d19c3d7da867d550fb1a7082bae02b43f9c5e00
Rich Doan <richd@indeed.com>
2016-07-04 12:04:33 -0500

    created Box widget

--- jooglechart/widgets.py
+++ jooglechart/widgets.py
@@ -310,6 +310,64 @@ class Button(object):
 
         display(HTML(self.render(include_common)))
 
+
+class Box(object):
+    
+    def __init__(self, *objects, **kwargs):
+
+        self._objects = objects
+        self._content_strings = []
+        self._div_styles = {}
+        self._div_id = None
+        self._display = kwargs.pop("display", "default")
+        self._vertical_align = kwargs.pop("vertical_align", "default")
+
+    def add_div_styles(self, style_dict = None, **kwargs):
+        """
+        pass styles for the chart div in a dictionary or as keyword arguments
+        """
+
+        if self._div_styles == None:
+            self._div_styles = {}
+
+        if style_dict == None and not kwargs:
+            # user is resetting styles
+
+            self.div_styles = {}
+        else:
+            if style_dict == None:
+                style_dict = {}
+            if kwargs:
+                style_dict.update(kwargs)
+            _add_dict_to_dict(self._div_styles, style_dict)
+
+    def render(self, include_common=None):
+
+        num = get_joogle_object_counter()
+        self._div_id = "joogle_box_" + str(num)
+
+
+        context = {}
+        context['box'] = self
+        context['callback_name'] = 'doStuff_' + str(num)
+
+        # call common context method before rendering all child
+        # objects
+        set_common_on_context(context, include_common)
+
+        for obj in self._objects:
+            if isinstance(obj, str):
+                self._content_strings.append(obj)
+            else:
+                self._content_strings.append(obj.render(include_common=False))
+
+
+        return j2_env.get_template('top_box.html').render(context).encode('utf-8')
+
+    def show(self, include_common=None):
+
+        display(HTML(self.render(include_common)))
+
 class TableLegend(object):
     
     
