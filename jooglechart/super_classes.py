3bbe14d4e1f6ccd3b638a5b37e3bb42023ee91cd
Rich Doan <richd@indeed.com>
2016-09-11 10:38:01 -0500

    abstract logic from AddDivStyles

--- jooglechart/super_classes.py
+++ jooglechart/super_classes.py
@@ -4,7 +4,7 @@ Created on Sep 10, 2016
 @author: richd
 '''
 
-from utils import get_joogle_object_counter, set_common_on_context, j2_env, _add_dict_to_dict, JoogleChartsException
+from utils import get_joogle_object_counter, set_common_on_context, j2_env, _add_styles
 
 from IPython.display import display, HTML
 
@@ -12,40 +12,20 @@ from IPython.display import display, HTML
 Drop-in modules only provide common methods for subclasses.  They don't need
 an __init__ method or instance variables.
 """
+    
+
 class AddDivStyles(object):
     
     """
     Module for dropping add_div_styles into Joogle objects
     """
 
-    def replace_underscores_with_dashes(self, d):
-        
-        new_dict = {}
-        
-        for key in d:
-            new_key = key.replace('_', '-')
-            new_dict[new_key] = d[key]
-        return new_dict
-
-    def add_div_styles(self, style_dict = None, **kwargs):
+    def add_div_styles(self, style_dict = None, **kwargs):        
         """
         pass styles for the chart div in a dictionary or as keyword arguments
         """
-
-        if self._div_styles == None:
-            self._div_styles = {}
-
-        if style_dict == None and not kwargs:
-            # user is resetting styles
-
-            self._div_styles = {}
-        else:
-            if style_dict == None:
-                style_dict = {}
-            if kwargs:
-                kwargs = self.replace_underscores_with_dashes(kwargs)
-                style_dict.update(kwargs)
-            _add_dict_to_dict(self._div_styles, style_dict)
+        _add_styles(self, "_div_styles", style_dict, **kwargs)
+        
 
 
 class ContainerRender(object):
@@ -57,7 +37,7 @@ class ContainerRender(object):
     def render(self, include_common=None):
 
         num = get_joogle_object_counter()
-        self._div_id = "joogle_box_" + str(num)
+        self._div_id = self._div_prefix + str(num)
 
         context = {}
         context[self._context_name] = self
