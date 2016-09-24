dc922191f1e3b24775a13d6cabececb9c52d1fd1
Rich Doan <richd@indeed.com>
2016-09-24 14:51:01 -0500

    add agg_chart_receivers.js

---
+++ jooglechart/jinja_templates/js/agg_chart_receivers.js
@@ -0,0 +1,47 @@
+		{% for receiver in agg_chart._chart._receivers %}
+
+		function remove_rows(dt, rows) {
+			var num_rows = dt.getNumberOfRows();
+			for (var i = num_rows - 1; i >= 0; i--) {
+				if ($.inArray(i, rows) < 0) {
+					dt.removeRow(i);			
+				}
+			}
+		}
+
+		// -------------- Add chartwrapper receivers, if any -------------
+		(function(key, chart, chart_div_id) {
+			var func = function(data) {
+				if (data.sonar_key === key && document.getElementById(chart_div_id) !== null) {
+					
+					// The sonar message must the main chart's data table and view.  The view
+					// will have the rows visible after filtering.
+					// Make of copy of the datatable and trim it by the filtered rows.  Then make
+					// an aggregation datatable and draw the chart.
+					
+					var arr = data.sonar_value;
+					var source_dt = arr[0];
+					var source_view = arr[1];
+					var dt_copy = new google.visualization.DataTable(source_dt.toJSON());
+
+					// source_view.rows is null, then the chart is unfiltered, so skip this
+					if (source_view !== null && source_view.rows !== null) {
+						remove_rows(dt_copy, source_view.rows);
+					}
+					
+					var dt = google.visualization.data.group(dt_copy, [2],
+						[{'column': 3, 'aggregation': google.visualization.data.avg, 'type': 'number', 'label': 'avg'},
+						{'column': 3, 'aggregation': google.visualization.data.count, 'type': 'number', 'label': 'count'},
+						]
+					)
+					
+					chart.setDataTable(dt);
+					chart.draw();
+				}
+			}
+			joogle_globals.sonar.add_handler(func);
+			joogle_globals.sonar.add_one_time_ready_poller(key, func, chart);
+		})("{{ receiver.key }}", {{ agg_chart._chart.name }}, "{{ agg_chart._chart.chart_div_id }}");
+
+		{% endfor %}
+		
