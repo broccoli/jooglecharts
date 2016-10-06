d483e94e9b693eb480cf0c092579f800d4e9c334
Rich Doan <richd@indeed.com>
2016-10-06 11:33:37 -0500

    set pandas version in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='jooglechart',
-    version='0.1.1',
+    version='0.1.2',
     packages=['jooglechart',],
     package_data={'jooglechart': ['jinja_templates/*.html', '*.ipynb', 'jinja_templates/js/*.js', 'jinja_templates/css/*.css']},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
@@ -20,7 +20,7 @@ setup(
         'Jinja2==2.8',
 #         'gviz_api==1.8.2',
         'IPython[notebook]>=4.0.0, <4.1.0',
-        'pandas',
+        'pandas==0.15.2',
     ],
 #     dependency_links = ['git+https://github.com/google/google-visualization-python.git#egg=gviz_api-1.8.2'],
     )
